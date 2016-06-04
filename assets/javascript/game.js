042ab65d09f23df80ed957ce58c029b464402270
Ethan Favia <efavia215@live.com>
2016-06-04 13:11:32 -0400

    rethememing

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+var goku = {
+	name: "Goku",
+	health: 175,
+	attackPower: 18,
+	attckAdd: 18,
+	counterAtk: 21,
+}
+
+var frieza = {
+	name: "Frieza",
+	health: 140,
+	attackPower: 25,
+	attackAdd: 25,
+	counterAtk: 24,
+}
+
+var vegeta = {
+	name: "Vegeta",
+	health: 180,
+	attackPower: 16,
+	attackAdd: 16, 
+	counterAtk: 19,
+}
+
+var piccolo = { 
+	name: "Piccolo",
+	health: 250, 
+	attackPower: 12,
+	attackAdd: 12, 
+	counterAtk: 14, 
+}
+
+function choosePlayer() {
+
+	
+}
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+
+
+
+});
