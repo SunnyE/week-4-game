d87a0ecdc300a2639fe5c475213f9c1c18e5224f
Ethan Favia <efavia215@live.com>
2016-06-06 13:47:34 -0400

    working on game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,16 +30,52 @@ var piccolo = {
 	counterAtk: 14, 
 }
 
+var chosen; 
+var badGuy;
 function choosePlayer() {
+		$('.chosen').append(this);
+		chosen = $(this).attr('id');
+		$('.enemey').append($('charlist'));
 
 	
 }
 
+function pickEnemy {
+		$('.toFight').append(this);
+		badGuy = $(this).attr('id');
+	}
 
 
+function readyTheField () {
+	if (chosen == "piccolo") {
+		chosen = piccolo;
+	} 
+	if (chosen == "vegeta") {
+		chosen = vegeta;
+	} 
+	if (chosen == "goku") {
+		chosen = goku;
+	} 
+	if (chosen == "frieza") {
+		chosen = frieza;
+	} 
+	if (badGuy == "frieza") {
+		badGuy = frieza;
+	} 
+	if (badGuy == "goku") {
+		badGuy = goku;
+	}
+	if (badGuy == "vegeta") {
+		badGuy = vegeta;
+	}
+	if (badGuy == "piccolo") {
+		badGuy = piccolo;
+	}  
+}
 
-
-
+function attack () { 
+	
+}
 
 
 
